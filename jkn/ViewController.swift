c849f819563d29f66cb196d300fa4c9f2681152a
Yumeto Sasamori <tomato1206@i.softbank.jp>
2018-06-16 01:29:16 +0900

    Initial Commit

---
+++ jkn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jkn
+//
+//  Created by Yumeto Sasamori on 2018/06/16.
+//  Copyright © 2018年 Yumeto Sasamori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
