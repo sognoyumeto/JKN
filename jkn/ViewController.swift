a72dd1468aa0f5b1044aa06d9851f896cdb717d4
Yumeto Sasamori <tomato1206@i.softbank.jp>
2018-06-16 14:05:23 +0900

    JKN application without any Icon

--- jkn/ViewController.swift
+++ jkn/ViewController.swift
@@ -20,6 +20,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    // じゃんけん（数字）
+    var answerNumber = 0
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        
+        //0,1,2の数値をランダムに算出（乱数）
+        
+        // arc4random_uniform()の戻り値はUInt32だがSwiftの標準的な聖地型Intにキャストする（変換）する
+        answerNumber = Int(arc4random_uniform(3))
+        
+    
+        if answerNumber == 0 {
+         // グー
+            
+        answerLabel.text = "GUGUGGUGU"
+        answerImageView.image = UIImage(named:"gu")
+   
+        }else if answerNumber == 1 {
+         // チョキ
+            
+        answerLabel.text = "ちょきんちょきん"
+        answerImageView.image = UIImage(named:"choki")
+            
+            
+        }else if answerNumber == 2 {
+         // パー
+            
+        answerLabel.text = "ぱぱぱぱぱ"
+        answerImageView.image = UIImage(named:"pa")
+        }
+        
+      
 }
 
+
+}
